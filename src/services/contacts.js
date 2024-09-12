61a623575d0a4b4e57c9cb401cebb85e5ba93110
Irina Svetlova <irasvet1997@gmail.com>
2024-09-12 15:37:08 +0300

    return old name

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,4 +1,4 @@
-import { ContactsCollection } from '../db/models/test.js';
+import { ContactsCollection } from '../db/models/contacts.js';
 
 export const getAllContacts = async () => {
   const contacts = await ContactsCollection.find();
