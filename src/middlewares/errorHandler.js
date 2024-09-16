74bf6eee5e64d58ccfd677eb301527bc46453552
Irina Svetlova <irasvet1997@gmail.com>
2024-09-16 12:58:50 +0300

    Update errorHandler.js

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,20 +1,10 @@
-import { HttpError } from 'http-errors';
-
 export const errorHandler = (err, req, res, next) => {
-  if (err instanceof HttpError) {
-    res.status(err.status).json({
-      status: err.status,
-      message: err.name,
-      data: err,
-    });
-    return;
-  }
+  const { status = 500, message = 'Something went wrong' } = err;
 
-  res.status(500).json({
-    status: 500,
-    message: 'Something went wrong',
-    data: err.message,
+  res.status(status).json({
+    status,
+    message,
   });
 
-  next(err);
+  next();
 };
