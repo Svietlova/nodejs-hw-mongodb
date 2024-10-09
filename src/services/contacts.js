95c582aca18de22de26c228e13070187bd246189
Irina Svetlova <irasvet1997@gmail.com>
2024-10-10 01:16:28 +0300

    add email/password

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -41,13 +41,7 @@ export const getAllContacts = async ({
 };
 
 export const getContactById = async (contactId, userId) => {
-  // const contact = await ContactsCollection.findById(contactId, userId);
-  const contact = await ContactsCollection.findOne({
-    _id: contactId,
-    userId: userId,
-  });
-
-  return contact;
+  return await ContactsCollection.findOne({ _id: contactId, userId });
 };
 
 export const createContact = async (payload) => {
